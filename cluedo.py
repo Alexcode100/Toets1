07687a7d8faa51cecb91a577bd76ab719270ce8d
Alexcode100 <89846069+Alexcode100@users.noreply.github.com>
2021-09-27 13:23:20 +0100

    Yes

--- cluedo.py
+++ cluedo.py
@@ -17,10 +17,34 @@ SpecVraag3 = 'Heeft u een snor? (ja/nee)\n---> '
 #Kolonel
 
 #Dominee
-SpecVraag4 = 'Bent u een dominee in het echt?'
-SpecVraag5 = ''
-SpecVraag6 = ''
+SpecVraag4 = 'Bent u een dominee in het echt? (ja/nee)\n---> '
+SpecVraag5 = 'Woont u in een dorp? (ja/nee)\n---> '
+SpecVraag6 = 'Heeft u een baard? (ja/nee)\n---> '
 #Dominee
+
+#Professor
+SpecVraag7 = 'Bent u een archeoloog? (ja/nee)\n---> '
+SpecVraag8 = 'Bent u goed in geschiedenis? (ja/nee)\n---> '
+SpecVraag9 = 'Heeft u haar? (ja/nee)\n---> '
+#Professor
+
+#MW. De Wit
+SpecVraag10 = 'Heeft u ervaring met koken? (ja/nee)\n---> '
+SpecVraag11 = 'Heeft u voor iemand gewerkt? (ja/nee)\n---> '
+SpecVraag12 = 'Heeft u lang haar? (ja/nee)\n---> '
+#MW. De Wit
+
+#Rosa
+SpecVraag13 ='Vind u zichzelf knap? (ja/nee)\n---> '
+SpecVraag14 ='Heeft u ervaring met het zijn van een filmster? (ja/nee)\n---> '
+SpecVraag15 ='Hoelang bent u?(Antwoord in CM) (ja/nee)\n---> '
+#Rosa
+
+#M.W Blaauw
+SpecVraag16 = ''
+SpecVraag17 = ''
+SpecVraag18 = ''
+#M.W Blaauw
 
 #Vragen
 input('---------------------------------------------------\nHallo! Begin hier met uw auditie voor de Talents action\nTyp \033[1mStart\033[0m om te beginnen!\n---> ')
@@ -35,6 +59,22 @@ if Geslacht == "Man":
     Vraag1 = input(SpecVraag1)
     Vraag2 = input(SpecVraag2)
     Vraag3 = input(SpecVraag3)
+    Vraag4 = input(SpecVraag4)
+    Vraag5 = input(SpecVraag5)
+    Vraag6 = input(SpecVraag6)
+    Vraag7 = input(SpecVraag7)
+    Vraag8 = input(SpecVraag8)
+    Vraag9 = input(SpecVraag9)
+else:
+    Vraag10 = input(SpecVraag10)
+    Vraag11 = input(SpecVraag11)
+    Vraag12 = input(SpecVraag12)
+    Vraag13 = input(SpecVraag13)
+    Vraag14 = input(SpecVraag14)
+    Vraag15 = input(SpecVraag15)
+    Vraag16 = input(SpecVraag16)
+    Vraag17 = input(SpecVraag17)
+    Vraag18 = input(SpecVraag18)
     
 
 
